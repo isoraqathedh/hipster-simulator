b41317ef88806f191fbd774e218384a146d71a44
unknown <isoraqathedh.zorethan@gmail.com>
2014-11-28 00:07:44 +0000

    Rename classes to shorter and more consistent names.

--- hipsters.lisp
+++ hipsters.lisp
@@ -9,7 +9,7 @@
 
 ;;; Classes
 
-(defclass hipster-town-snapshot ()
+(defclass town-snapshot ()
   ((ticks :initform 0
           :initarg :time
           :accessor ticks
@@ -26,7 +26,7 @@
                         :documentation "How likely would any inidividual be nonconformist at any given time."))
   (:documentation "A hipster town at a particular time."))
 
-(defclass hipster-town-delay (hipster-town-snapshot)
+(defclass delayed-town (town-snapshot)
   ((period :initform 2
            :initarg :period
            :accessor period
@@ -46,30 +46,30 @@
                
 (defun build-new-town (population &key (hipsterish-tendency 2/3) (styles #(#\# #\.)))
   "Builds a new town with a population of population, seeding with the two types of clothes."
-  (make-instance 'hipster-town-snapshot
+  (make-instance 'town-snapshot
                  :population (generate-random-town population styles)
                  :styles styles
                  :hipsterish-tendency hipsterish-tendency))
 
 (defun build-delayed-town (population period &key (hipsterish-tendency 2/3) (styles #(#\# #\.)))
-  (make-instance 'hipster-town-delay
+  (make-instance 'delayed-town
                  :population (generate-random-town population styles)
                  :period period
                  :history (make-array (list period))
                  :hipsterish-tendency hipsterish-tendency
                  :styles styles))
 
-(defmethod initialize-instance :after ((instance hipster-town-delay) &key)
+(defmethod initialize-instance :after ((instance delayed-town) &key)
   (setf (aref (town-history instance) (mod (ticks instance) (period instance))) (population instance)))
 
 ;;; Common class methods
 
-(defmethod print-object ((object hipster-town-snapshot) stream)
+(defmethod print-object ((object town-snapshot) stream)
   (with-accessors ((ticks ticks) (population population) (tendency hipsterish-tendency) (styles styles)) object
     (print-unreadable-object (object stream :type t)
       (format stream "~a ~4,2f%-hipsters with styles ~a @ t = ~a" (length population) (* tendency 100) (coerce styles 'list) ticks))))
 
-(defmethod print-object ((object hipster-town-delay) stream)
+(defmethod print-object ((object delayed-town) stream)
   (with-accessors ((ticks ticks) (population population) (tendency hipsterish-tendency) (styles styles) (period period)) object
     (print-unreadable-object (object stream :type t)
       (format stream  "~a ~4,2f%-hipsters with styles ~a, period ~a @ t = ~a" (length population) (* tendency 100) (coerce styles 'list) period ticks))))
@@ -84,21 +84,21 @@
 
 (defgeneric style-popularity (town)
   (:documentation "Determines the popularity of each style in the town.")
-  (:method ((town hipster-town-snapshot))
+  (:method ((town town-snapshot))
     (loop with stats = (loop for style across (styles town)
                              collect (cons style 0))
           for i in (population town)
           do (incf (cdr (assoc i stats)))
           finally (return stats)))
-  (:method ((town hipster-town-delay))
+  (:method ((town delayed-town))
     (call-next-method
-     (make-instance 'hipster-town-snapshot
+     (make-instance 'town-snapshot
                     :styles (styles town)
                     :population (aref (town-history town) (mod (1+ (ticks town)) (period town)))))))
 
 (defgeneric survey (town)
   (:documentation "Counts what styles the inhabitant sees.")
-  (:method ((town hipster-town-snapshot))
+  (:method ((town town-snapshot))
     (loop with style-list = (style-popularity town)
           for (style) in style-list
           for i = (copy-tree style-list)
@@ -109,7 +109,7 @@
 (defgeneric deconform (town)
   (:documentation "Simulates the selection of styles of all hipsters.
 In this case, they will attempt at random any style on the less popular half of the style.")
-  (:method ((town hipster-town-snapshot))
+  (:method ((town town-snapshot))
     (let ((candidate-styles (survey town)))
       (mapcar #'(lambda (style-of-self)
                   (cond
@@ -122,7 +122,7 @@ In this case, they will attempt at random any style on the less popular half of 
                     ((true-with-probability 10/11) style-of-self)
                     (t (aref (styles town) (random (length (styles town)))))))
               (population town))))
-  (:method ((town hipster-town-delay))
+  (:method ((town delayed-town))
     (if (numberp (aref (town-history town) (mod (1+ (ticks town)) (period town))))
         (mapcar 
          ;; While the history fills up, randomly wander, changing fashions 1/6 of the time
@@ -148,8 +148,8 @@ In this case, they will attempt at random any style on the less popular half of 
 
 (defgeneric tick (town)
   (:documentation "Nondestructively returns the next iteration of the simulation.")
-  (:method ((town hipster-town-snapshot))
-    (make-instance 'hipster-town-snapshot
+  (:method ((town town-snapshot))
+    (make-instance 'town-snapshot
                    :time                (1+ (ticks town))
                    :population          (deconform town)
                    :styles              (styles town)
@@ -157,17 +157,17 @@ In this case, they will attempt at random any style on the less popular half of 
           
 (defgeneric tick! (town)
   (:documentation "Destructively modifies a snapshot to become the next iteration of the simulation.")
-  (:method ((town hipster-town-snapshot))
+  (:method ((town town-snapshot))
     (setf (population town) (deconform town))
     (incf (ticks town)))
-  (:method :after ((town hipster-town-delay))
+  (:method :after ((town delayed-town))
     (setf (aref (town-history town) (mod (ticks town) (period town))) (population town))))
 
 ;;; Reporting methods
 
 (defgeneric print-population (town &key stream limit specific-style)
   (:documentation "Prints out the first [limit] members in the population along with a generation number wearing the given style if non-nil.")
-  (:method ((town hipster-town-snapshot) &key (stream t) limit specific-style)
+  (:method ((town town-snapshot) &key (stream t) limit specific-style)
     (with-accessors ((age ticks) (members population)) town
       (if specific-style
           (format stream "~&~3d: ~{~a~}~:[~;…~]" age (mapcar #'(lambda (a) (if (char-equal specific-style a)
@@ -178,7 +178,7 @@ In this case, they will attempt at random any style on the less popular half of 
 
 (defgeneric print-popularity (town &key stream limit)
   (:documentation "Prints out the first [limit] styles and its percentage popularity, along with a genreation number.")
-  (:method ((town hipster-town-snapshot) &key (stream t) limit)
+  (:method ((town town-snapshot) &key (stream t) limit)
     (with-accessors ((age ticks) (members population) (styles styles)) town
       (format stream "~&~3d:  " age)
       (loop for i across styles
@@ -190,7 +190,7 @@ In this case, they will attempt at random any style on the less popular half of 
 ;;; When seeing the development of the town, we need to repeat things a lot.
 (defmacro with-town-iterator ((var town &key (times 20) (tick-time :before)) &body body)
   `(let ((,var ,town))
-     (assert (typep ,var 'hipster-town-snapshot))
+     (assert (typep ,var 'town-snapshot))
      (assert (typep ,tick-time '(member :before :after)))
      (loop repeat ,times
            do (progn
